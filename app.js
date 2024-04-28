f7223a982a50ee3b49b2c6dab89ee6c5a16a80cd
saltedSt0ne <166182914+saltedSt0ne@users.noreply.github.com>
2024-04-27 21:57:36 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// app.js
+document.addEventListener('DOMContentLoaded', function() {
+  const sidenavElems = document.querySelectorAll('.sidenav');
+  const sidenavInstances = M.Sidenav.init(sidenavElems);
+});
